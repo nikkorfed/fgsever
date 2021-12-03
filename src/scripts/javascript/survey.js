f0366dac9464b11f8726b860af6f1f972b14ff02
Никита Корнилов <nikkorfed@gmail.com>
2021-12-04 01:32:31 +0300

    Add all questions to survey.

--- src/scripts/javascript/survey.js
+++ src/scripts/javascript/survey.js
@@ -2,17 +2,15 @@ const memory = {};
 
 $("section.advanced-survey form").on("click", "input", function (e) {
   if (e.target.name === "Оценка условий труда") {
-    const target = $(e.target).parents(".question").next();
+    const target = $(e.target).parents(".answers").find(".clarify");
+    target.removeClass("hidden");
     if (+e.target.value < 5) {
-      target.removeClass("hidden");
       if (memory["Почему меньше пяти"]) {
-        $(e.target).parents(".question").after(memory["Почему меньше пяти"]);
+        $(e.target).parents(".answers").append(memory["Почему меньше пяти"]);
         delete memory["Почему меньше пяти"];
       }
     } else {
-      if (!memory["Почему меньше пяти"]) {
-        memory["Почему меньше пяти"] = target.detach();
-      }
+      if (!memory["Почему меньше пяти"]) memory["Почему меньше пяти"] = target.detach();
     }
   }
 
@@ -28,21 +26,50 @@ $("section.advanced-survey form").on("click", "input", function (e) {
 
     let shouldClarify = false;
     elements.each(function () {
-      if (conditions.some((condition) => this.value === condition)) shouldClarify = true;
+      if (conditions.includes(this.value)) shouldClarify = true;
     });
 
     const target = $(e.target).parents(".answers").find(`textarea[name="Улучшение рабочих условий (детали)"]`);
 
+    target.removeClass("hidden");
     if (shouldClarify) {
-      target.removeClass("hidden");
       if (memory["Улучшение рабочих условий (детали)"]) {
         $(e.target).parents(".answers").append(memory["Улучшение рабочих условий (детали)"]);
         delete memory["Улучшение рабочих условий (детали)"];
       }
     } else {
-      if (!memory["Улучшение рабочих условий (детали)"]) {
-        memory["Улучшение рабочих условий (детали)"] = target.detach();
+      if (!memory["Улучшение рабочих условий (детали)"]) memory["Улучшение рабочих условий (детали)"] = target.detach();
+    }
+  }
+
+  if (e.target.name === "Желаете официально трудоустроиться") {
+    const target = $(e.target).parents(".answers").find(`textarea[name="Желаете официально трудоустроиться (другое)"]`);
+    target.removeClass("hidden");
+    if (e.target.value === "Другой ответ") {
+      if (memory["Желаете официально трудоустроиться (другое)"]) {
+        $(e.target).parents(".answers").append(memory["Желаете официально трудоустроиться (другое)"]);
+        delete memory["Желаете официально трудоустроиться (другое)"];
       }
+    } else {
+      if (!memory["Желаете официально трудоустроиться (другое)"])
+        memory["Желаете официально трудоустроиться (другое)"] = target.detach();
+    }
+  }
+
+  if (e.target.name === "Где провести новогодний корпоратив") {
+    const target = $(e.target)
+      .parents(".answers")
+      .find(`textarea[name="Где провести новогодний корпоратив (свой ответ)"]`);
+    console.log(target);
+    target.removeClass("hidden");
+    if (e.target.value === "Свой ответ") {
+      if (memory["Где провести новогодний корпоратив (свой ответ)"]) {
+        $(e.target).parents(".answers").append(memory["Где провести новогодний корпоратив (свой ответ)"]);
+        delete memory["Где провести новогодний корпоратив (свой ответ)"];
+      }
+    } else {
+      if (!memory["Где провести новогодний корпоратив (свой ответ)"])
+        memory["Где провести новогодний корпоратив (свой ответ)"] = target.detach();
     }
   }
 });
