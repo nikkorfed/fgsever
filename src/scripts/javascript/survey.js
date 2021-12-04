4a60a150354a08adec52e33bb7c8c747721c3f22
Никита Корнилов <nikkorfed@gmail.com>
2021-12-04 03:26:43 +0300

    Make survey 'step-by-step' and with validation.

--- src/scripts/javascript/survey.js
+++ src/scripts/javascript/survey.js
@@ -1,12 +1,77 @@
 const memory = {};
 
+$("section.advanced-survey form").on("click", ".button:not([type=submit])", function () {
+  // Валидация данных
+  const step = $(this).parents(".step");
+  const answers = step.find(".answers");
+
+  let allValid = true;
+  answers.each(function () {
+    let radioValid = true;
+    const radioInputs = $(this).find("input[type=radio]");
+    if (radioInputs.length > 0) {
+      radioValid = false;
+      for (let input of radioInputs) if (input.checked) radioValid = true;
+      if (!radioValid) {
+        if (!$(this).prev(".required").length)
+          $(this).before('<p class="required">Пожалуйста выберите один из вариантов.</p>');
+      }
+    }
+
+    let checkboxValid = true;
+    const checkboxInputs = $(this).find("input[type=checkbox]");
+    if (checkboxInputs.length > 0) {
+      checkboxValid = false;
+      for (let input of checkboxInputs) if (input.checked) checkboxValid = true;
+      if (!checkboxValid) {
+        if (!$(this).prev(".required").length)
+          $(this).before('<p class="required">Пожалуйста выберите один или несколько вариантов.</p>');
+      }
+    }
+
+    let textValid = true;
+    const textInputs = $(this).find("textarea:not(.hidden)");
+    if (textInputs.length > 0) {
+      for (let input of textInputs) {
+        console.log($(input).val());
+        if (!$(input).val()) textValid = false;
+        if (!$(input).val() && !$(input).prev(".required").length)
+          $(input).before('<p class="required">Пожалуйста введите ответ в поле ниже.</p>');
+      }
+    }
+
+    console.log("textValid", textValid);
+    if (!radioValid || !checkboxValid || !textValid) allValid = false;
+  });
+
+  console.log("allValid", allValid);
+  if (allValid === false) return;
+
+  // Переключение вопроса
+  if ($(this).hasClass("next")) {
+    $(this).parents(".step").removeClass("active");
+    $(this).parents(".step").next().addClass("active");
+  } else if ($(this).hasClass("back")) {
+    $(this).parents(".step").removeClass("active");
+    $(this).parents(".step").prev().addClass("active");
+  }
+});
+
+$("section.advanced-survey form").on("change", "textarea", function () {
+  if ($(this).val()) $(this).prev(".required").remove();
+});
+
 $("section.advanced-survey form").on("click", "input", function (e) {
+  if ($(e.target).attr("type") === "radio" || $(e.target).attr("type") === "checkbox") {
+    $(e.target).parents(".answers").prev(".required").remove();
+  }
+
   if (e.target.name === "Оценка условий труда") {
-    const target = $(e.target).parents(".answers").find(".clarify");
+    const target = $(e.target).parents(".answers").next(".clarify");
     target.removeClass("hidden");
     if (+e.target.value < 5) {
       if (memory["Почему меньше пяти"]) {
-        $(e.target).parents(".answers").append(memory["Почему меньше пяти"]);
+        $(e.target).parents(".answers").after(memory["Почему меньше пяти"]);
         delete memory["Почему меньше пяти"];
       }
     } else {
