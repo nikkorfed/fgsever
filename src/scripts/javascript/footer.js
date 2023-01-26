0da7f981d333d96d7421235bc9439dd329762ab2
Никита Корнилов <nikkorfed@gmail.com>
2023-01-26 17:23:32 +0500

    Keep only calculators scripts as modules.

--- src/scripts/javascript/footer.js
+++ src/scripts/javascript/footer.js
@@ -5,13 +5,13 @@ $(".special-buttons .close-button").click(function () {
 
 if (!$.cookie("special-buttons-closed")) showSpecialButtons();
 
-export function showSpecialButtons() {
+function showSpecialButtons() {
   $(".special-buttons").removeClass("hidden");
   $(".special-buttons .close-button").removeClass("menu-white");
   $(".special-buttons .close-button").addClass("cross-black");
 }
 
-export function hideSpecialButtons() {
+function hideSpecialButtons() {
   $.cookie("special-buttons-closed", true, { expires: 1, path: "/" });
   $(".special-buttons").addClass("hidden");
   $(".special-buttons .close-button").removeClass("cross-black");
