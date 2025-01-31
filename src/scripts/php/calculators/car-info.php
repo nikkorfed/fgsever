a7c435dc5e6a0f6efa162d6a773169ee2c6cd12c
Никита Корнилов <nikkorfed@gmail.com>
2025-01-31 15:45:57 +0500

    Replace car image URL with local server image.

--- src/scripts/php/calculators/car-info.php
+++ src/scripts/php/calculators/car-info.php
@@ -7,6 +7,7 @@ include_once 'car-images.php';
 if (isset($_REQUEST['vin']) && !isset($_REQUEST['mileage'])) {
   if (isset($_REQUEST['data']) && $_REQUEST['data'] == 'images') $result = requestCarImages($_REQUEST['vin']);
   else $result = requestCarInfo($_REQUEST['vin'], $_REQUEST['from']);
+
   header('content-type: application/json; charset=UTF-8');
   echo json_encode($result, JSON_UNESCAPED_UNICODE);
 }
@@ -26,7 +27,17 @@ function requestCarInfo($vin, $from) {
   $html = curl_exec($ch);
   curl_close($ch);
 
-  return json_decode($html, true);
+  $html = json_decode($html, true);
+
+  if (isset($html['image']) && mb_strpos($html['image'], '/aos-parser/images') !== false) {
+    $protocol = !empty($_SERVER['HTTPS']) ? 'https://' : 'http://';
+    $host = $_SERVER['HTTP_HOST'];
+
+    $fullVin = $html['vin'];
+    $html['image'] = $protocol . $host . "/scripts/php/calculators/car-images.php?vin=$fullVin&image=image";
+  }
+
+  return $html;
 }
 
 // Запрос изображений автомобиля
