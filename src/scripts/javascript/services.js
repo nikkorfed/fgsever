7dc299d2c129442a23b791abfbb9bc6b7210b898
Никита Корнилов <nikkorfed@gmail.com>
2025-04-17 01:13:58 +0500

    Adjust styles and behavior for radiators wash service.

--- src/scripts/javascript/services.js
+++ src/scripts/javascript/services.js
@@ -1,3 +1,3 @@
 $("section.services:not(.options)").on("click", ".services-item", function () {
-  location.href = $(this).find(".button-simple").attr("href");
+  location.href = $(this).find("a[class^=button]").attr("href");
 });
